142afad54ea82ea385c4d8262de6a532d3206080
mattifestation <mattgraeber@gmail.com>
2015-02-03 21:47:05 -0500

    Added parameters back to the original Invoke-Shellcode

--- CodeExecution/Invoke-Shellcode.ps1
+++ CodeExecution/Invoke-Shellcode.ps1
@@ -5,4 +5,50 @@
 # many scripts that have this path hardcoded. If you don't like it,
 # fork PowerSploit and host it yourself.
 
+function Invoke-Shellcode
+{
+
+[CmdletBinding( DefaultParameterSetName = 'RunLocal', SupportsShouldProcess = $True , ConfirmImpact = 'High')] Param (
+    [ValidateNotNullOrEmpty()]
+    [UInt16]
+    $ProcessID,
+    
+    [Parameter( ParameterSetName = 'RunLocal' )]
+    [ValidateNotNullOrEmpty()]
+    [Byte[]]
+    $Shellcode,
+    
+    [Parameter( ParameterSetName = 'Metasploit' )]
+    [ValidateSet( 'windows/meterpreter/reverse_http',
+                  'windows/meterpreter/reverse_https',
+                  IgnoreCase = $True )]
+    [String]
+    $Payload = 'windows/meterpreter/reverse_http',
+    
+    [Parameter( ParameterSetName = 'ListPayloads' )]
+    [Switch]
+    $ListMetasploitPayloads,
+    
+    [Parameter( Mandatory = $True,
+                ParameterSetName = 'Metasploit' )]
+    [ValidateNotNullOrEmpty()]
+    [String]
+    $Lhost = '127.0.0.1',
+    
+    [Parameter( Mandatory = $True,
+                ParameterSetName = 'Metasploit' )]
+    [ValidateRange( 1,65535 )]
+    [Int]
+    $Lport = 8443,
+    
+    [Parameter( ParameterSetName = 'Metasploit' )]
+    [ValidateNotNull()]
+    [String]
+    $UserAgent = 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
+    
+    [Switch]
+    $Force = $False
+)
+
 throw 'Something terrible may have just happened and you have no idea what because you just arbitrarily download crap from the Internet and execute it.'
+}
