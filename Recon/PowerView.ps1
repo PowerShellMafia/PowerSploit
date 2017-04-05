f2a9cb2ecc28495ae7e31e228e14185dc1d6614e
Jon Cave <jon.cave@mwrinfosecurity.com>
2017-04-05 13:15:54 +0100

    The DFS subfunctions only accept a single $Domain
    Specifying the type as an array of strings results in an error when
    passing on the parameter to Get-DomainSearcher:
    
        C:\> Get-DomainDFSShare -Domain test
        Get-DomainSearcher : Cannot process argument transformation on parameter 'Domain'. Cannot convert value to type System.String.
        At PowerView.ps1:10242 char:46

--- Recon/PowerView.ps1
+++ Recon/PowerView.ps1
@@ -9915,7 +9915,7 @@ function Get-DomainDFSShare {
 .SYNOPSIS
 
 Returns a list of all fault-tolerant distributed file systems
-for the current (or specified) domain.
+for the current (or specified) domains.
 
 Author: Ben Campbell (@meatballs__)  
 License: BSD 3-Clause  
@@ -9930,7 +9930,7 @@ The server data is parsed appropriately and returned.
 
 .PARAMETER Domain
 
-Specifies the domain to use for the query, defaults to the current domain.
+Specifies the domains to use for the query, defaults to the current domain.
 
 .PARAMETER SearchBase
 
@@ -10213,7 +10213,7 @@ A custom PSObject describing the distributed file systems.
         function Get-DomainDFSShareV1 {
             [CmdletBinding()]
             Param(
-                [String[]]
+                [String]
                 $Domain,
 
                 [String]
@@ -10292,7 +10292,7 @@ A custom PSObject describing the distributed file systems.
         function Get-DomainDFSShareV2 {
             [CmdletBinding()]
             Param(
-                [String[]]
+                [String]
                 $Domain,
 
                 [String]
